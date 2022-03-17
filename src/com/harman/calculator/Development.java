810cdf68de100ffc6aa0e5414c910a30f624de06
Nithinrab <nithinrab08@gmail.com>
2022-03-17 08:38:36 +0530

    subtraction completed

--- src/com/harman/calculator/Development.java
+++ src/com/harman/calculator/Development.java
@@ -2,8 +2,11 @@ package com.harman.calculator;
 
 public class Development {
     public static void main(String[] args) {
+        Subtraction subc=new Subtraction();
         int a= 10,b=30,c;
         c=a+b;
         System.out.println(c);
+        int result = subc.sub(a,b);
+        System.out.println(result);
     }
 }
