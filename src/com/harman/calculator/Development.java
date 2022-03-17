e4f767bdc856928c2ca2104b87e295baf1970c76
Nithinrab <nithinrab08@gmail.com>
2022-03-17 08:10:10 +0530

    addition

---
+++ src/com/harman/calculator/Development.java
@@ -0,0 +1,9 @@
+package com.harman.calculator;
+
+public class Development {
+    public static void main(String[] args) {
+        int a= 10,b=30,c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
